697317091bb86a3eb40448e8d88874159541fb11
Kesara Rathnayake <kesara@fq.nz>
2023-04-13 22:00:49 +1200

    feat: Add paging to search results

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from waitress import serve
+from redis.commands.search.query import Query
 import json
 import os
 import redis
@@ -11,9 +12,17 @@ app = Flask(__name__)
 redis_db = redis.from_url(os.environ['REDIS_URL'])
 
 
+# try to create index if missing
+try:
+    redis_db.execute_command(f'FT.CREATE {DOC_INDEX} ON JSON SCHEMA $.title AS title TEXT $.abstract AS abstract TEXT $.authors[:][*] AS name TEXT')
+except Exception:
+    pass
+
+
+
 @app.route('/')
 def hello_world():
-    return 'try /get/<document_id> and /search/<term>'
+    return 'try /get/<document_id> and /search/<query>'
 
 
 @app.route('/get/<document_id>')
@@ -25,9 +34,12 @@ def get(document_id):
         return jsonify({'error': 'Document not found'}), 404
 
 
-@app.route('/search/<term>')
-def search(term):
-    results = redis_db.ft(DOC_INDEX).search(term)
+@app.route('/search/<query>')
+def search(query):
+    offset = request.values.get('offset', 0)
+    limit = request.values.get('limit', 1000)
+    results = redis_db.ft(DOC_INDEX).search(Query(query).paging(offset, limit))
+    total = results.total
     docs = []
     for doc in results.docs:
         data = json.loads(doc.json)
@@ -37,7 +49,7 @@ def search(term):
             'abstract': data['abstract'],
             'json_url': f'{BASE_URL}/get/{doc.id}',
             })
-    return jsonify({'documents': docs})
+    return jsonify({'total_results': total, 'documents': docs})
 
 
 if __name__ == '__main__':
